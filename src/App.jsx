ec44aaa97b5a69e142087921f285cfb833d47af8
onwr <onurkurkaya@gmail.com>
2025-03-02 13:26:20 +0300

    sa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import SignIn from "@pages/SignIn";
 import Home from "./pages/Home";
+import Loader from "./layout/Loader";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/hesap/giris-kayit" element={<SignIn />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <AnimatePresence mode="wait">{loading && <Loader />}</AnimatePresence>
+      {!loading && (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/hesap/giris-kayit" element={<SignIn />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
-}
+};
 
 export default App;
